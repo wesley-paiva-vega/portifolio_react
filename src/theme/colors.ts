7dbd6a6baddcf06bef0183947bfbb9ffbeb23d86
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-08-04 11:42:24 -0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    brand_color: "#00DF5E",
+    dark_10: "#171717",
+    dark_20: "#212121",
+    dark_30: "#333333",
+    dark_40: "#828282",
+    dark_50: "#F9F9F9",
+    message: "#2962FF",
+    success: "#0BB07B",
+    warning: "#FFCE52",
+    error: "#F03D3D",
+    information: "#E0E0E0"
+}
