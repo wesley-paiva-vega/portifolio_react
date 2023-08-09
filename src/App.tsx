abef166e0d021e15f6c4827ff666315ff7c58d34
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-08-09 13:49:06 -0300

    ajuste main

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import Button from "./components/Button";
-import Header from "./components/Header";
 import Main from "./components/Main";
-import NameSection from "./components/NameSection";
-import GlobalStyle, { SectionButtons } from "./styles";
+import GlobalStyle from "./styles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Main>
-        <Header />
-        <NameSection />
-        <SectionButtons>
-          <Button
-            color="dark_10"
-            borderColor="light"
-            backgroundColor="brand_color"
-            title="Download CV"
-          />
-          <Button
-            backgroundColor="dark_10"
-            borderColor="none"
-            color="dark_10"
-            title="Download CV"
-          />
-        </SectionButtons>
-      </Main>
+      <Main />
     </>
   );
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,30 @@
-import { ReactNode } from "react";
 import * as Styled from "./styles";
+import Header from "../Header";
+import NameSection from "../NameSection";
+import { SectionButtons } from "../../styles";
+import Button from "../Button";
 
-type MainProps = {
-  children: ReactNode;
-};
-
-const Main = ({ children }: MainProps): JSX.Element => {
-  return <Styled.Main>{children}</Styled.Main>;
+const Main = (): JSX.Element => {
+  return (
+    <Styled.Main>
+      <Header />
+      <NameSection />
+      <SectionButtons>
+        <Button
+          color="dark_10"
+          borderColor="light"
+          backgroundColor="brand_color"
+          title="Download CV"
+        />
+        <Button
+          backgroundColor="dark_10"
+          borderColor="none"
+          color="dark_10"
+          title="Download CV"
+        />
+      </SectionButtons>
+    </Styled.Main>
+  );
 };
 
 export default Main;
